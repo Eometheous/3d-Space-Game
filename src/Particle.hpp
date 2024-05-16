a74d9190c8da8fbbb60133ae87c0842a1af9070f
Eometheous <jonathan.sthomas@icloud.com>
2024-05-16 15:15:16 -0700

    updated partcle class

--- src/Particle.hpp
+++ src/Particle.hpp
@@ -1,27 +1,31 @@
 //
 //  Particle.hpp
-//  3dSpaceGame
 //
-//  Created by Jonathan Thomas on 5/16/24.
+//  Created by Jonathan Thomas on 3/24/24.
 //
 
 #ifndef Particle_hpp
 #define Particle_hpp
 
-#pragma once
 #include "GameObject.h"
-
 
 #endif /* Particle_hpp */
 
 class Particle: public GameObject {
 public:
+    ofVec3f forces;
+    
     float age;
-    float birthtime;
+    float birthTime;
     
     ofShader shader;
     
     Particle();
+    Particle(ofVec3f position, ofVec3f force);
+    
     void update();
+    void integrate();
     void draw();
+    void setPosition(ofVec3f pos);
+    void setVelocity(ofVec3f f);
 };
