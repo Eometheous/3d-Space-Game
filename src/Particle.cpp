dce72550418967e1920735f4fc9b90b7883b71ce
Eometheous <jonathan.sthomas@icloud.com>
2024-05-16 15:15:16 -0700

    updated partcle class

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -1,34 +1,71 @@
 //
 //  Particle.cpp
-//  3dSpaceGame
 //
-//  Created by Jonathan Thomas on 5/16/24.
+//  Created by Jonathan Thomas on 3/24/24.
 //
 
 #include "Particle.hpp"
 
+/**
+ The default constructor for a particle
+ */
 Particle::Particle() {
-    shader.load("shaders/shader");
-    
+    position.set(0, 0);
+    velocity.set(0, 0);
+    acceleration.set(0, 0);
+    forces.set(0, -20);
     age = 0;
-    birthtime = ofGetElapsedTimeMillis() / 1000;
-    position = ofVec2f();
+    birthTime = ofGetElapsedTimeMillis();
 }
 
+/**
+ Creates a particle with a starting position and applied force
+ */
+Particle::Particle(ofVec3f pos, ofVec3f force) {
+    position.set(pos);
+    velocity.set(0, 0);
+    acceleration.set(0, 0);
+    forces.set(force);
+}
+
+/**
+ Updates the particle's age and integrates it for a new position based on the forces applied to it
+ */
 void Particle::update() {
-    age = (ofGetElapsedTimeMillis() / 1000) - birthtime;
+    age = (ofGetElapsedTimeMillis()) - birthTime;
     integrate();
 }
 
+void Particle::integrate() {
+    float dt = 1.0 / ofGetFrameRate();
+    
+    position += velocity * dt;
+    
+    ofVec3f currentAcceleration = acceleration;
+    currentAcceleration += forces;
+    velocity += currentAcceleration * dt;
+    
+    forces.set(velocity * -.5);
+}
+
+/**
+ Draws a yellow sphere at the particles position
+ */
 void Particle::draw() {
-    shader.begin();
-    ofMatrix4x4 translation = ofMatrix4x4();
-    translation.translate(position);
-    
-    ofPushMatrix();
-    
-    ofMultMatrix(translation);
+    ofSetColor(ofColor::yellow);
     ofDrawSphere(position, .01);
-    ofPopMatrix();
-    shader.end();
+}
+
+/**
+ Updates the particle's position
+ */
+void Particle::setPosition(ofVec3f pos) {
+    position.set(pos);
+}
+
+/**
+ Updates the particle's velocity
+ */
+void Particle::setVelocity(ofVec3f f) {
+    velocity.set(f);
 }
