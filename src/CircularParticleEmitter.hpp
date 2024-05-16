43e0a45612d531bfd25556a05d7a4487244c00ab
Eometheous <jonathan.sthomas@icloud.com>
2024-05-16 14:13:05 -0700

    added CircularParticleEmitter

---
+++ src/CircularParticleEmitter.hpp
@@ -0,0 +1,13 @@
+//
+//  CircularParticleEmitter.hpp
+//  3dSpaceGame
+//
+//  Created by Jonathan Thomas on 5/16/24.
+//
+
+#ifndef CircularParticleEmitter_hpp
+#define CircularParticleEmitter_hpp
+
+#include <stdio.h>
+
+#endif /* CircularParticleEmitter_hpp */
